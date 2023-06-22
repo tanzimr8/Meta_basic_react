b9606990e037765062c5cf131a3ad7043d65fe62
Tanzim <tanzim008@gmail.com>
2023-06-22 00:07:53 -0230

    content in components added

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav(){
+    return (
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Articles</li>
+            <li>Contact</li>
+        </ul>
+    );
+}
+export default Nav;
